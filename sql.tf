c3cd206d7904c1c92e65adc58e533be50695e81b
Oleksiy Alex <oleksiyalex3@gmail.com>
2019-05-11 12:37:48 +0300

    [new] Add SQL database

---
+++ sql.tf
@@ -0,0 +1,42 @@
+data "null_data_source" "eschoolprod_mysql" {
+  count  = "${var.countnat}"
+  inputs = {
+    name  = "nat-${count.index + 1}"
+    value = "${element(google_compute_address.eschoolprod.*.address, count.index)}"
+    
+  }
+}
+
+resource "google_sql_database_instance" "eschoolprod" {
+    name               = "${var.prefix}-mysql"
+    region             = "${var.region}"
+    database_version   = "${var.database_version}"
+
+    settings {
+        tier             = "${var.tier}"
+        disk_autoresize  = "${var.disk_autoresize}"
+        disk_size        = "${var.disk_size}"
+        disk_type        = "${var.disk_type}"
+        ip_configuration {
+            ipv4_enabled = "true"
+            authorized_networks = [
+               "${data.null_data_source.eschoolprod_mysql.*.outputs}",
+            ]
+        }
+    }
+}
+resource "google_sql_database" "eschoolprod" {
+  name      = "${var.db_name}"
+  project   = "${var.project_name}"
+  instance  = "${google_sql_database_instance.eschoolprod.name}"
+  charset   = "${var.db_charset}"
+  collation = "${var.db_collation}"
+}
+
+resource "google_sql_user" "eschoolprod" {
+  name     = "${var.user_name}"
+  project  = "${var.project_name}"
+  instance = "${google_sql_database_instance.eschoolprod.name}"
+  host     = "${var.user_host}"
+  password = "${var.user_password}"
+}
--- variables.tf
+++ variables.tf
@@ -13,15 +13,17 @@ variable "project_name" {
   default     = "mydevops-234619"
 }
 
-variable "cluster" {
-  default = "mydevops-cluster"
-}
-
 variable "prefix" {
   description = "The prefix used for all resources"
   default = "eschool"
 }
 
+#Cluster
+variable "cluster" {
+  default = "mydevops-cluster"
+}
+
+#Network
 variable "ip_cidr_range" {
 	description = "The address_prefix used for subnetwork ip"
   default = "10.0.1.0/24"
@@ -31,3 +33,64 @@ variable "countnat" {
 	description = "The count nat used for prefix to nat external address"
   default = "1"
 }
+
+#DB
+variable database_version {
+  description = "The version of of the database. `MYSQL-5-7`"
+  default     = "MYSQL_5_7"
+}
+
+variable tier {
+  description = "The machine tier or type. See this page for supported tiers and pricing: https://cloud.google.com/sql/pricing"
+  default     = "db-g1-small"
+}
+
+variable disk_autoresize {
+  description = "Second Generation only. Configuration to increase storage size automatically."
+  default     = true
+}
+
+variable disk_size {
+  description = "Second generation only. The size of data disk, in GB. Size of a running instance cannot be reduced but can be increased."
+  default     = 10
+}
+
+variable disk_type {
+  description = "Second generation only. The type of data disk: `PD_SSD` or `PD_HDD`."
+  default     = "PD_SSD"
+}
+
+variable db_name {
+  description = "Name of the default database to create"
+  default     = "eschooldb"
+}
+
+variable db_charset {
+  description = "The charset for the default database"
+  default     = "utf8"
+}
+
+variable db_collation {
+  description = "The collation for the default database. Example for MySQL databases: 'utf8_general_ci', and Postgres: 'en_US.UTF8'"
+  default     = "utf8_general_ci"
+}
+
+variable user_name {
+  description = "The name of the default user"
+  default     = "root"
+}
+
+variable user_host {
+  description = "The host for the default user"
+  default     = "%"
+}
+
+variable user_password {
+  description = "The password for the default user"
+  default     = "IF095eSchool"
+}
+
+
+
+
+
